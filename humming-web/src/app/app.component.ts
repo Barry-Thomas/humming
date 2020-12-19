161ccb47d6dcb4432347ddb22e15a7795b02bb2e
Barry-Thomas <barryharoldthomas@gmail.com>
2020-12-20 01:44:02 +0400

    add angular and bootstrap

---
+++ humming-web/src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  title = 'Humming Web App';
+  name = 'Let"s see';
+}
